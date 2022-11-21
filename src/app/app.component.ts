05d0b9b7b34ac5ac6553b22c08f2afb68db2d3fd
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2022-11-21 11:10:36 +0300

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  password = "";
+
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  modifyLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+
+  }
+
+
+  modifyLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  modifyNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  modifySymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  buttonClick() {
+    const numbers = "1234567890"
+    const letters = "abcdefghijklmnopqrstuvwyz"
+    const symbols = "!@#$%^&*()"
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+
+    let generatedPassword = " ";
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+
+    this.password = generatedPassword;
+
+
+  }
+
 }
